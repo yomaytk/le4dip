2d41c5c99d11be20528ad287823266c0b07b0a48
Masashi <ym.atonement.oy999@gmail.com>
2019-12-19 15:33:28 +0900

    neuralnet1.py minor collection

--- le4/neuralnet1.py
+++ le4/neuralnet1.py
@@ -2,14 +2,15 @@
 import sys, os
 sys.path.append(os.pardir)  # 親ディレクトリのファイルをインポートするための設定
 import numpy as np
-from common.functions import sigmoid, softmax
 from dataset.mnist import load_mnist
 from PIL import Image
 
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))    
 
-def img_show(img):
-    pil_img = Image.fromarray(np.uint8(img))
-    pil_img.show()
+def softmax(x):
+    x = x - np.max(x)
+    return np.exp(x) / np.sum(np.exp(x))
 
 def init_network2d(I, d, M, C):
     np.random.seed(100)
@@ -19,16 +20,6 @@ def init_network2d(I, d, M, C):
     network["W2"] = np.random.normal(0, 1.0/M, (C, M))
     network["b2"] = np.random.normal(0, 1.0/M, (C))
     return network
-
-def init_network3d(B, I, d, M, C):
-    np.random.seed(100)
-    network = {}
-    network["W1"] = np.random.normal(0, 1.0/d, (M, d))
-    network["b1"] = np.random.normal(0, 1.0/d, (M))
-    network["W2"] = np.random.normal(0, 1.0/M, (C, M))
-    network["b2"] = np.random.normal(0, 1.0/M, (C))
-    return network
-
 
 if __name__ == "__main__":
     print("画像の選択...")
@@ -45,5 +36,5 @@ if __name__ == "__main__":
     y1 = sigmoid(np.dot(W1, y0) + b1)
     # 出力層の出力
     y2 = softmax(np.dot(W2, y1) + b2)
-    print(y2)
+    # print(y2)
     print(np.argmax(y2))
